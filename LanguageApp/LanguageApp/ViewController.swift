313f59fa63c10dd304b3b3dac2333522354e950a
Asude Nisa Tıraş <asudenisatiras@gmail.com>
2023-10-26 13:02:07 +0300

    Proje oluşturuldu.

---
+++ LanguageApp/LanguageApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LanguageApp
+//
+//  Created by Asude Nisa Tıraş on 26.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

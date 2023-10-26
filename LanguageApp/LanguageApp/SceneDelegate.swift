e8b5abe1e45e103f7585fc155fe4cdc54bcdc91a
Akgunahmet <ahmetakgun1703@gmail.com>
2023-10-26 14:35:15 +0300

    update

--- LanguageApp/LanguageApp/SceneDelegate.swift
+++ LanguageApp/LanguageApp/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = HomeViewController()
+        window.rootViewController = NativeLanguageViewController()
         window.makeKeyAndVisible()
 
         self.window = window
